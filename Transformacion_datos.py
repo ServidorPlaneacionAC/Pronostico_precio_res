3081538e2a3f4d12f7470570cc983a20f70e9cd6
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2024-04-04 20:23:36 -0500

    corrección errores

--- Transformacion_datos.py
+++ Transformacion_datos.py
@@ -9,11 +9,11 @@ class transformacion:
         self.df['Periodo']=[datetime.strptime(f"{int(fila['Año'])} {int(fila['Semana'])}-1", "%Y %W-%w") for _, fila in self.df.iterrows()]
         self.df['Precio_Planta']=self.df['Cantidad_Reses']*self.df['Precio_Planta']
         
-        self.df = self.df.groupby(['PERIODO']).agg({
+        self.df = self.df.groupby(['Periodo']).agg({
             'Cantidad_Reses': 'sum',
             'Precio_Planta': 'sum'
         }).reset_index()
-        self.df['Precio_final']=self.df[' Precio en planta']/self.df[' Cantidad de Reses Liquidadas']
+        self.df['Precio_final']=self.df['Precio_Planta']/self.df['Cantidad_Reses']
         self.df=self.df[['Periodo','Precio_final']]
         self.df=self.df.set_index('Periodo')
         self.df = self.df[~self.df.index.duplicated(keep='first')]
