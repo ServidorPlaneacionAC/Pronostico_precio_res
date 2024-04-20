6225ef3c22fc268a8d7a81176cac39f722f58259
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2024-04-20 12:30:08 -0500

    dd

--- Transformacion_datos.py
+++ Transformacion_datos.py
@@ -89,74 +89,33 @@ class pronosticar_precio_reses:
         import numpy as np
         import statsmodels.api as sm
 
-
-        inicio_serie_real=self.df.shape[0]-self.elementos_mostrar
-
-        acf = sm.tsa.acf(self.df['Precio_final'], nlags=len(self.df)-1)
-
-        # Graficamos el ACF
-        plt.figure(figsize=(10, 6))
-        plt.stem(acf)
-        plt.xlabel('Lag')
-        plt.ylabel('Autocorrelación')
-        plt.title('Gráfico de Autocorrelación (ACF)')
-        plt.show()
-        st.pyplot()
-
-        st.write('''Si ves picos en los retrasos múltiplos de un cierto número (por ejemplo, picos en los lags 7, 14, 21, etc., para datos semanales), esto sugiere la presencia de estacionalidad en ese intervalo de tiempo. Por ejemplo, en datos semanales, un pico en el lag 7 podría indicar estacionalidad semanal.''')
-
-        diferencias = self.df['Precio_final'].diff()
-
-        # Graficamos las diferencias
-        plt.figure(figsize=(10, 6))
-        plt.plot(self.df.index, diferencias, marker='o', linestyle='-')
-        plt.axhline(y=0, color='gray', linestyle='--')  # Línea horizontal en y=0 para referencia
-        plt.xlabel('Fecha')
-        plt.ylabel('Diferencia')
-        plt.title('Gráfico de Diferenciación para Identificar Tendencia')
-        plt.grid(True)
-        plt.show()
-        st.pyplot()
-
-        np.random.seed(0)
-        indice_tiempo = pd.date_range(start='2024-01-01', periods=100, freq='D')
-        serie = np.linspace(1, 100, 100) + np.random.normal(0, 5, 100)
-        df = pd.DataFrame({'valor': serie}, index=indice_tiempo)
-
-        # Calculamos las diferencias entre puntos de datos sucesivos
-        diferencias = df['valor'].diff()
-
-        # Graficamos las diferencias
-        plt.figure(figsize=(10, 6))
-        plt.plot(df.index, diferencias, marker='o', linestyle='-')
-        plt.axhline(y=0, color='gray', linestyle='--')  # Línea horizontal en y=0 para referencia
-        plt.xlabel('Fecha')
-        plt.ylabel('Diferencia')
-        plt.title('Gráfico de Diferenciación para Identificar Tendencia Lineal')
-        plt.grid(True)
-        plt.show()
-        st.pyplot()
         
-        datos_reales = self.df[inicio_serie_real:]
+        inicio_serie_real=self.df.shape[0]-self.elementos_mostrar
+        if st.button('Ver ACF de mis datos'):
+            acf = sm.tsa.acf(self.df['Precio_final'], nlags=len(self.df)-1)
+            # Graficamos el ACF
+            plt.figure(figsize=(10, 6))
+            plt.stem(acf)
+            plt.xlabel('Lag')
+            plt.ylabel('Autocorrelación')
+            plt.title('Gráfico de Autocorrelación (ACF)')
+            plt.show()
+            st.pyplot()
+            st.write('''Si ves picos en los retrasos múltiplos de un cierto número (por ejemplo, picos en los lags 7, 14, 21, etc., para datos semanales), esto sugiere la presencia de estacionalidad en ese intervalo de tiempo. Por ejemplo, en datos semanales, un pico en el lag 7 podría indicar estacionalidad semanal.''')
 
-        # Crear un array de índices de tiempo numéricos para la tendencia
-        indice_numerico = np.arange(len(datos_reales))
+        if st.button('Ver gráfico de diferenciación de mis datos'):
+            diferencias = self.df['Precio_final'].diff()
 
-        # Calcular la tendencia lineal
-        tendencia_coef = np.polyfit(indice_numerico, datos_reales, 1)
-        tendencia = np.poly1d(tendencia_coef)
-
-        # Graficar los datos con la línea de tendencia
-        plt.figure(figsize=(12, 6))
-        plt.plot(self.df.index[inicio_serie_real:], datos_reales, label='Datos reales', color='blue')
-        plt.plot(self.proximo_periodo, self.pronostico, label='Pronóstico', color='red')
-        plt.fill_between(self.proximo_periodo, self.intervalo_confianza[:, 0], self.intervalo_confianza[:, 1], color='pink', alpha=0.3)
-        plt.plot(self.df.index[inicio_serie_real:], tendencia(indice_numerico), linestyle='--', color='green', label='Tendencia')
-        plt.title('Comparación de pronóstico vs. datos reales')
-        plt.xlabel('Fecha')
-        plt.ylabel('Valor')
-        plt.legend()
-        st.pyplot()
+            # Graficamos las diferencias
+            plt.figure(figsize=(10, 6))
+            plt.plot(self.df.index, diferencias, marker='o', linestyle='-')
+            plt.axhline(y=0, color='gray', linestyle='--')  # Línea horizontal en y=0 para referencia
+            plt.xlabel('Fecha')
+            plt.ylabel('Diferencia')
+            plt.title('Gráfico de Diferenciación para Identificar Tendencia')
+            plt.grid(True)
+            plt.show()
+            st.pyplot()
 
         plt.figure(figsize=(12, 6))
         plt.plot(self.df.index[inicio_serie_real:], self.df[inicio_serie_real:], label='Datos reales', color='blue')
